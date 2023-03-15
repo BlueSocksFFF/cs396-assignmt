f1a60a347406b518e4900bb6a8193489c7b1f689
DianeZh <yifanzhang2024@u.northwestern.edu>
2023-03-14 19:52:27 -0500

    updated world.sdf

--- solution.py
+++ solution.py
@@ -46,8 +46,6 @@ class SOLUTION:
         os.system("del fitness"+str(self.myID)+".txt")
 
     def Create_World(self):
-        while not os.path.exists("world.sdf"):
-            time.sleep(0.01)
         pyrosim.Start_SDF("world.sdf")
         pyrosim.End()
 
